da12752861e6bfaa859d0ad9db575be60f396169
Moe <badb28609@gmail.com>
2024-10-28 18:42:18 +0200

    very important update !!!

--- Assets/scripts/Github test.cs
+++ Assets/scripts/Github test.cs
@@ -7,7 +7,7 @@ public class moe : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("jjjjjj");
     }
 
     // Update is called once per frame
