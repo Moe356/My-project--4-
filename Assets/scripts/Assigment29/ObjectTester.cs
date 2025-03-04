e7eaf9de4886cf6a8a2cce99818bf7c7a3115f8f
Moe <badb28609@gmail.com>
2025-03-04 18:37:18 +0200

    /quick changes

---
+++ Assets/scripts/Assigment29/ObjectTester.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class ObjectTester : MonoBehaviour
+    {
+        void Start()
+        {
+            var obj = new CustomObject(1, "Example");
+            print(obj);
+        }
+    }
