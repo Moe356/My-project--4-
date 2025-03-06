88071581f395eaf9a81d6ad46ceb93988c405135
Moe <badb28609@gmail.com>
2025-03-07 00:10:28 +0200

    /quick changes

--- Assets/scripts/Assignment35/ListMethodsExample.cs
+++ Assets/scripts/Assignment35/ListMethodsExample.cs
@@ -8,26 +8,20 @@ namespace Assignment35
         void Start()
         {
             List<int> numbers = new List<int> { 3, 1, 4, 1, 5, 9 };
-
-            numbers.Sort(CompareDescending);
+            numbers.Sort((x, y) => y.CompareTo(x));
 
             Debug.Log(string.Join(", ", numbers));
+
             List<int> numbers2 = new List<int> { 3, 1, 4, 1, 5, 9, 2, 6 };
 
-
-            List<int> filteredNumbers = numbers2.FindAll(IsEven);
+            List<int> filteredNumbers = numbers2.FindAll(x => x % 2 == 0);
 
             Debug.Log(string.Join(", ", filteredNumbers));
-
         }
 
         int CompareDescending(int x, int y)
         {
             return y.CompareTo(x);
-        }
-        bool IsEven(int number)
-        {
-            return number % 2 == 0;
         }
     }
 }
